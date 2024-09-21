d647e1dc1106c9411b89650a6086cdbe534b1f96
Keysie Dela Cruz <161782851+keysieee@users.noreply.github.com>
2024-09-21 19:18:33 +0800

    FILES

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = require('./routes/routes'); 
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use('/', routes);
+
+app.listen(3000, () => {
+    console.log('server running on http://localhost:3000');
+});
